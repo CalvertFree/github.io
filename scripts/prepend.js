98c7da5b06d1fb6f04e20ea71c184ab88c2fa217
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-01-12 14:13:19 -0800

    scripts

---
+++ scripts/prepend.js
@@ -0,0 +1,55 @@
+import { readFileSync, openSync, writeFileSync, writeSync, close } from 'node:fs';
+import { format } from 'node:util';
+import { ensureFile } from 'fs-extra';
+import testFirstLine from './testFirstLine.js';
+import { getFirst, readFirstLine } from './getFirstLine.js';
+
+function formatDate(date, formatStr) {
+ const map = {
+  mm: ('' + date.getMonth() + 1).padStart(2, "0"),
+  dd: ('' + date.getDate()).padStart(2, "0"),
+  y2: date.getFullYear().toString().slice(-2),
+  yyyy: date.getFullYear()
+ }
+
+ return formatStr.replace(/mm|dd|y2|yyyy/gi, matched => map[matched])
+}
+
+const fileNameBase = 'message';
+const today = new Date();
+const fileDate = formatDate(today, 'yyyy_mm_dd');
+const fileName = format(`%s_${fileNameBase}.markdown`, fileDate);
+
+writeFileSync(fileName, '', 'utf8', 'a');
+async function ensureFileElf(fileName) {
+ try {
+  await ensureFile(fileName)
+  console.log('Await Success!')
+ } catch (err) {
+  console.error(err)
+ }
+}
+ensureFileElf(fileName)
+
+console.log('barDate:', fileName);
+
+const frontMatter = format(`---
+layout: post
+date: %s
+title: %s
+---
+`, fileDate, 'placeHolder');
+
+const firstLine = await getFirst(fileName);
+
+if (firstLine != '---') {
+ const data = readFileSync(fileName)
+ const fd = openSync(fileName, 'w+')
+ const insert = Buffer.from(frontMatter);
+ writeSync(fd, insert, 0, insert.length, 0)
+ writeSync(fd, data, 0, data.length, insert.length)
+ close(fd, (err) => {
+  if (err) throw err;
+ });
+}
+
